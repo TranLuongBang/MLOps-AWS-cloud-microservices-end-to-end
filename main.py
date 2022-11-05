5b44ffc8516116ed893ea3c7460ece03076b6a2c
BangTran <bangtranluong195@gmail.com>
2022-11-05 02:49:21 +0100

    adding more tests

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki, search_wiki, phrase
 
-result = wiki()
-print(result)
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """search for wikipedia"""
+
+    results = search_wiki(value)
+    return {"total": results}
+
+
+@app.get("/wiki/{name}")
+async def get_wiki(name: str):
+    """retrieve wiki page"""
+
+    results = wiki(name)
+    return {"total": results}
+
+
+@app.get("/phrase/{name}")
+async def phrase_wiki(name: str):
+    """retrieve wiki page and phrase it"""
+
+    results = phrase(name)
+    return {"total": results}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
